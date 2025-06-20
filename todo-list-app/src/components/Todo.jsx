1ac9ff9308539d865bd4d1e7446628ff0bb78fab
Vidisha Srivastava <vidishasrivastava2001@gmail.com>
2025-06-20 11:37:00 +0530

    handling form submissions and state management using the useState hook

--- todo-list-app/src/components/Todo.jsx
+++ todo-list-app/src/components/Todo.jsx
@@ -1,15 +1,28 @@
 import "./Todo.css";
 import { useState } from "react";
+import { MdCheck, MdDeleteForever } from "react-icons/md";
+
 
 export const Todo = () => {
   const [inputValue, setInputValue] = useState("");
+  const [task,setTask] = useState([]);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
   const handleFormSubmit = (event) => {
     event.preventDefault(); 
-  };// Prevent the default form submission behavior 
+
+    if(!inputValue) return;
+    if(task.includes(inputValue))
+    {
+      setInputValue("");
+      return;
+    }
+    setTask((prev) => [...prev, inputValue]);
+    setInputValue(""); 
+    
+  }; 
   return (
     <section className="todo-container">
       <header>
@@ -17,7 +30,7 @@ export const Todo = () => {
       </header>
 
       <section className="form">
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <div>
             <input
               type="text"
@@ -33,6 +46,21 @@ export const Todo = () => {
             </button>
           </div>
         </form>
+      </section>
+      <section className="myUnOrdList">
+        <ul>
+          {task.map((curTask, index) => (
+           
+            <li key={index} className="todo-item">
+              <span>{curTask}</span>
+              <button className="check-btn">           
+                <MdCheck /> </button>
+              <button className="delete-btn">
+                <MdDeleteForever />
+              </button>
+            </li>
+          ))}
+        </ul>
       </section>
     </section>
   );
